91396141cd39f4a5ef25dd95db4bba102a1965e2
Intan Devia Putri <intandeviap@gmail.com>
2024-10-09 13:25:27 +0700

    Update Latihan_2.java

--- Tes_Koneksi/Latihan_2.java
+++ Tes_Koneksi/Latihan_2.java
@@ -6,24 +6,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Latihan_2 {
-    static final String DB_Url = "jdbc:mysql://localhost:3306/db_Unidha3";
-    public static void main(String[]args) {
+
+    static final String DB_Url= "jdbc:mysql://localhost:3306/db_unidha3";
+    public static void main(String[]args){
     }
     
-    private static Connection Tes_Konek;
-    public static Connection DB_Konek () throws SQLException,ClassNotFoundException {
+    private static Connection Test_Konek;
+    public static Connection DB_Konek()throws SQLException, ClassNotFoundException {
         try {
-            Driver myDriver =new com.mysql.jdbc.Driver();
+            Driver myDriver = new com.mysql.jdbc.Driver();
             DriverManager.registerDriver(myDriver);
             System.out.println("Proses Deteksi Driver Berhasil");
             
-            final java.sql.Connection tersambung = DriverManager.getConnection(DB_Url,"root","");
+            final java.sql.Connection tersambung = DriverManager.getConnection (DB_Url, "root", "");
             System.out.println("Koneksi Database Berhasil");
-            
         } catch (final SQLException ex) {
-            System.out.println ("Koneksi Database Gagal") ;
-            
-        } 
-    return Test_Konek;
-    }
+            System.out.println("Koneksi Database Gagal");
+        }
+    return Test_Konek;    
+    }    
 }
